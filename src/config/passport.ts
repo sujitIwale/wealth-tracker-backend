e8c156a9f1ff600cfda50aa959a7ad8d0b22ba73
sujitIwale <sujit.iwale5115@gmail.com>
2025-03-24 14:44:11 +0530

    adding conosle log

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -17,12 +17,16 @@ passport.deserializeUser(async (id: string, done) => {
   }
 });
 
+console.log(process.env.GOOGLE_CLIENT_ID || '',
+  process.env.GOOGLE_CLIENT_SECRET || '',
+  process.env.GOOGLE_CALLBACK_URL || '',)
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || '',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
